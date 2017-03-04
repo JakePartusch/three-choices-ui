dbace252259e82f38ce0933e3a7e2b9243a0f11e
Jake Partusch <jakepartusch@gmail.com>
2017-03-03 20:16:07 -0600

    Adding routing and some styling tweaks

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Headers, RequestOptions } from '@angular/http';
-import { Http }          from '@angular/http';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-add',
@@ -13,14 +13,24 @@ export class AddComponent implements OnInit {
   }
   name: String = ''
   errorMessage: String = ''
+  isSuccessfulSave = false;
 
   ngOnInit() {
   }
 
   submit() {
     this.addRestaurant()
-                   .subscribe(
-                     error =>  this.errorMessage = <any>error);
+           .subscribe(
+             response => this.saveSuccess(),
+             error =>  this.errorMessage = <any>error);
+  }
+
+  saveSuccess() {
+    this.name = '';
+    this.isSuccessfulSave = true;
+    setTimeout(() => {
+          this.isSuccessfulSave = false;
+    }, 3000);
   }
 
   addRestaurant() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'add',  component: AddComponent },
+  { path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -16,6 +26,7 @@ import { NavComponent } from './nav/nav.component';
     NavComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
